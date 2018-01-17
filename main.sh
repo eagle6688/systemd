e908a9a3f7eedaee8a025e648e5c88fb41ecebe9
Aldwin Su <fangyansu@163.com>
2018-01-17 22:14:07 +0800

    update

--- main.sh
+++ main.sh
@@ -1,3 +1,33 @@
 echo "Start main.sh"
-sleep 2s
-echo "Stop main.sh"
+asd=Hello
+qwe=World
+zxc="I am Eagle!"
+message="I need sleep 3 seconds!"
+
+if [ $ASD ]
+then
+    asd=$ASD
+fi
+
+if [ $QWE ]
+then
+    qwe=$QWE
+fi
+
+if [ $ZXC ]
+then
+    zxc=$ZXC
+fi
+
+echo "$asd $qwe, ${zxc}"
+
+while true
+do
+
+echo $message
+echo $message >> /home/service/systemd/text.txt
+sleep 3s
+
+done
+
+echo "End the mission!"
